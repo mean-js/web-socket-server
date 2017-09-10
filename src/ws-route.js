ff6462534c9c66511799cddb7b5250fc91476aa0
santosh <santosh.ece06@gmail.com>
2017-09-10 18:30:17 +0530

    ws server

--- src/ws-route.js
+++ src/ws-route.js
@@ -1,11 +1,30 @@
 var moment = require("moment");
+const http = require('http');
+const WebSocket = require('ws');
 
 function WsRoute(app) {
+    const server = http.createServer(app);
+    const wss = new WebSocket.Server({ server });
+
 
     app.get("/ws", function(req, res, next) {
         var resobj = { "title": "Affixus WS", "ts": moment().toDate() };
         res.json(resobj);
-    })
+    });
+
+
+    wss.on('connection', function connection(ws, req) {
+        const location = url.parse(req.url, true);
+        // You might use location.query.access_token to authenticate or share sessions
+        // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+        ws.on('message', function incoming(message) {
+            console.log('received: %s', message);
+        });
+
+        ws.send('something');
+    });
+
 
 };
 
